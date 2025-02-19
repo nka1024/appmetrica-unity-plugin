c2689bc33bcf86f37ddd242b577126f3cd4e8e03
nka1024 <nka1024@gmail.com>
2025-02-19 15:57:45 +0700

    fix build

--- Runtime/Plugins/iOS/AMAUAppMetricaProxy.m
+++ Runtime/Plugins/iOS/AMAUAppMetricaProxy.m
@@ -21,14 +21,14 @@ void amau_setUnityLogDelegate(AMAULogCallbackDelegate logger)
 {
     _unityLogDelegate = logger;
     if (_unityLogDelegate != nil){
-        _unityLogDelegate(@"amau_setUnityLogDelegate")
+        _unityLogDelegate(@"amau_setUnityLogDelegate");
     }
 }
 
 void amau_activate(char *configJson)
 {
     if (_unityLogDelegate != nil){
-        _unityLogDelegate(@"Trying to activate appmetrica with configuration: %s", configJson)
+        _unityLogDelegate(@"Trying to activate appmetrica with configuration: %s", configJson);
     }
     AMAAppMetricaConfiguration *config = amau_deserializeAppMetricaConfiguration(configJson);
     if (config != nil) {
